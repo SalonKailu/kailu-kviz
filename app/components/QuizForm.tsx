3b53a768be7ec94dee46cec8bd180f35c31189f3
SalonKailu <info@kailu.cz>
2024-12-08 23:43:06 +0100

    pokus o opravu bileho pisma v mobilu

--- app/components/QuizForm.tsx
+++ app/components/QuizForm.tsx
@@ -432,13 +432,13 @@ A skvělý {' '}
        <CardContent className="p-4">
          <SectionHeader currentQuestion={currentQuestion} />
          <div className={`mb-6 transition-opacity duration-300 ${isTransitioning ? 'opacity-0' : 'opacity-100'}`}>
-         <h2 className="text-md font-bold mb-2 text-gray-900 dark:text-white">
-             {currentQ.title}
-           </h2>
+         <h2 className="text-md font-bold mb-2 dark:text-white">
+  {currentQ.title}
+</h2>
 
            {currentQ.type === 'info' ? (
              <div className="space-y-10">
-               <p className="text-sm leading-[1.8]">{currentQ.content}</p>
+               <p className="text-sm leading-[1.8] dark:text-white">{currentQ.content}</p>
                <div className="flex justify-end mt-6">
                  <CustomButton
                    onClick={() => handleQuestionChange(currentQuestion + 1)}
@@ -466,7 +466,7 @@ A skvělý {' '}
     />
                    <Label
                      htmlFor={`option-${index}`}
-                     className="text-sm cursor-pointer leading-[1.8] text-gray-900 dark:text-white"
+                     className="text-sm cursor-pointer leading-[1.8] dark:text-white"
                    >
                      {typeof option === 'object' ? option.text : option}
                    </Label>
@@ -520,7 +520,7 @@ A skvělý {' '}
                              />
                              <Label
                                htmlFor={`option-${index}`}
-                               className="text-sm cursor-pointer leading-[1.8]"
+                               className="text-sm cursor-pointer leading-[1.8] dark:text-white"
                              >
                                {currentQ.options[index]}
                              </Label>
