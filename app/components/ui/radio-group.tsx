73b01e8770b9af52b9d87b1b2ba9065a62839443
SalonKailu <info@kailu.cz>
2024-12-08 23:01:18 +0100

    Fix radio buttons styling

--- app/components/ui/radio-group.tsx
+++ app/components/ui/radio-group.tsx
@@ -18,11 +18,15 @@ const RadioGroupItem = React.forwardRef(({...props }, ref) => {
   return (
     <RadioGroupPrimitive.Item
       ref={ref}
-      className="aspect-square h-4 w-4 rounded-full border border-slate-200 border-slate-900 text-slate-900 ring-offset-white focus:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+      className="relative w-4 h-4 rounded-full border-2 border-[#faa4a6] bg-white inline-block
+        before:content-[''] before:block before:w-2 before:h-2 before:rounded-full 
+        before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2
+        data-[state=checked]:before:bg-[#faa4a6]
+        focus:outline-none focus:ring-2 focus:ring-[#faa4a6] focus:ring-offset-2"
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <div className="h-2.5 w-2.5 rounded-full bg-slate-900" />
+        <div className="h-2.5 w-2.5 rounded-full bg-[#faa4a6]" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
